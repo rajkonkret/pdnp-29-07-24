9455e784e9fa9a17ddb4e7f929c78ceeed907375
radek <rajkonkret660@gmail.com>
2024-07-29 13:56:30 +0200

    [1.5] update formaty.py

--- formaty.py
+++ formaty.py
@@ -16,3 +16,45 @@ print("Witaj {}, masz teraz {} lat.".format(user, wiek))  # Witaj Tomek, masz te
 print(f"Witaj {user}, masz teraz {wiek} lat.")  # Witaj Tomek, masz teraz 39 lat.
 # f - fstring - sformatowany tekst
 
+# zaokrągla przy wyświetlaniu
+print("Używamy wersji pythona %i" % 3)  # Używamy wersji pythona 3
+print("Używamy wersji pythona %f" % 3.9)  # Używamy wersji pythona 3.900000
+print("Używamy wersji pythona %.1f" % 3.9)  # Używamy wersji pythona 3.9
+print("Używamy wersji pythona %.2f" % 3.9)  # Używamy wersji pythona 3.90
+print("Używamy wersji pythona %.0f" % 3.9)  # Używamy wersji pythona 4
+print("Używamy wersji pythona %.f" % 3.9)  # Używamy wersji pythona 4
+print("Wynik = %.2f" % 3.876)  # Wynik = 3.88
+x = 3.14
+print("Zero miejsc po przecinku %.f" % x)  # Zero miejsc po przecinku 3
+print("X nadal się nie zmmieniło", x)  # X nadal się nie zmmieniło 3.14
+y = round(x)
+print("y=", y)
+print("x=", x)
+# y= 3
+# x= 3.14
+
+x = 3.1415
+print(round(x, 2))  # 3.14
+
+print(f"Używamy wersji pythona {wersja}")  # Używamy wersji pythona 3.900001
+print(f"Używamy wersji pythona {wersja:.1f}")  # Używamy wersji pythona 3.9
+print(f"Używamy wersji pythona {wersja:.2f}")  # Używamy wersji pythona 3.90
+print(f"Używamy wersji pythona {wersja:.0f}")  # Używamy wersji pythona 4
+
+print(f'{user:>10}')  # "     Tomek"
+print(f'{user:<20}')  # "Tomek               "
+print(f"{user:^25}")  # "          Tomek          "
+
+print(liczba)  # 123456789123
+print(f"NAsza duża liczba {liczba:,}")  # NAsza duża liczba 123,456,789,123
+print(f"NAsza duża liczba {liczba:,}".replace(",", "."))
+print(f"NAsza duża liczba {liczba:,}".replace(",", " "))
+# NAsza duża liczba 123.456.789.123
+# NAsza duża liczba 123 456 789 123
+
+liczba_2 = 123_456_789_123
+print(liczba_2)
+print(type(liczba_2))  # <class 'int'>
+
+# 15_000_000
+# 15000000
