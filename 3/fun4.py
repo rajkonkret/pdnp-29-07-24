ddb000cb14fe39a7cfc163b5e1ed38a6c7d39585
radek <rajkonkret660@gmail.com>
2024-07-31 14:26:43 +0200

    [3.15] add fun4

---
+++ 3/fun4.py
@@ -0,0 +1,17 @@
+# funkcja wewnętrzna, zagnieżdżona
+
+def fun1():
+    print("To jest fun1")
+
+    def fun2():
+        print("To jest fun2")
+
+    return fun2  # zwróćenie adresu fun2 (referencji)
+
+
+fun1()  # To jest fun1
+print()
+xFun = fun1()
+print(type(xFun))  # <class 'function'>
+print(xFun)  # <function fun1.<locals>.fun2 at 0x00000228AEAD98A0>
+xFun()  # To jest fun2
