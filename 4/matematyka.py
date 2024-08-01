54ab6c6a018318642ab833d2e495055c5568cdff
radek <rajkonkret660@gmail.com>
2024-08-01 10:29:47 +0200

    [4.5] add matematyka.py

---
+++ 4/matematyka.py
@@ -0,0 +1,9 @@
+import math
+
+pi = math.pi
+print(pi)  # 3.141592653589793
+
+angle_radians = math.radians(90)
+print(math.cos(angle_radians))  # 6.123233995736766e-17
+
+print(math.sqrt(25))  # float, 5.0
