f36cf7a9a612fbc507038cc042601cc990988dd7
radek <rajkonkret660@gmail.com>
2024-08-02 14:51:32 +0200

    [5.20] add wykres3.py

---
+++ 5/wykres3.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import mpld3
+
+labels = ["Jabłko", "Banany", 'Winogrona', 'Pomarańćze']
+sizes = [30, 25, 20, 45]
+colors = ['red', 'blue', 'purple', 'yellow']
+
+plt.pie(sizes, labels=labels, colors=colors, shadow=True, startangle=90, explode=(0.1, 0, 0, 0),
+        autopct='%1.1f%%')
+
+plt.title("Wykres kołowy")
+plt.axis('equal')
+
+plt.show()
+
+html_str = mpld3.save_html(plt.gcf(), 'plot.html')
+
+# with open('plot.html', "w") as f:
+#     f.write(html_str)
